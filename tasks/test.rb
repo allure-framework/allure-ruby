b57482753445fc121c76809d4906a7394b83d6c9
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2019-05-16 10:04:11 +0300

    Add task output

--- tasks/test.rb
+++ tasks/test.rb
@@ -7,6 +7,7 @@ ADAPTORS = %w[allure-ruby-commons allure-cucumber].freeze
   desc "Run #{task_name} for all projects"
   task task_name do
     errors = ADAPTORS.each_with_object([]) do |adaptor, a|
+      puts "Executing #{task_name} for #{adaptor}"
       system("cd #{adaptor} && #{$PROGRAM_NAME} #{task_name}") || a << adaptor
     end
     raise Exception.new("Errors in #{errors.join(', ')}") unless errors.empty?
