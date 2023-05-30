ae019564c2869194408eab1a9522361be5bfdae1
Andrejs <andrejs.cunskis@gmail.com>
2023-05-30 10:19:28 +0300

    Correctly handle failure to load testplan.json (#495)
    * Correctly handle failure to load testplan.json
    
    * Remove codeclimate configuration
    
    * Simplify if condition and fix spec
    
    * Add additional specs

--- allure-ruby-commons/spec/unit/testplan_spec.rb
+++ allure-ruby-commons/spec/unit/testplan_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe Allure::TestPlan do
+describe Allure::TestPlan, :aggregate_failures do
   let(:test_ids) { ["test case id, aka allure id"] }
   let(:test_names) { ["some unique id or selector that can be used to run particular test case"] }
 
@@ -8,6 +8,10 @@ describe Allure::TestPlan do
     described_class.instance_variable_set(:@tests, nil)
     described_class.instance_variable_set(:@test_ids, nil)
     described_class.instance_variable_set(:@test_names, nil)
+
+    :@test_plan_path.tap do |var|
+      described_class.send(:remove_instance_variable, var) if described_class.instance_variable_defined?(var)
+    end
   end
 
   around do |example|
@@ -18,10 +22,8 @@ describe Allure::TestPlan do
     let(:path) { "#{Dir.pwd}/spec/fixtures/test_plan/correct/testplan.json" }
 
     it "correct testplan.json" do
-      aggregate_failures do
-        expect(described_class.test_ids).to eq(test_ids)
-        expect(described_class.test_names).to eq(test_names)
-      end
+      expect(described_class.test_ids).to eq(test_ids)
+      expect(described_class.test_names).to eq(test_names)
     end
   end
 
@@ -29,21 +31,26 @@ describe Allure::TestPlan do
     let(:path) { "#{Dir.pwd}/spec/fixtures/test_plan/malformed/testplan.json" }
 
     it "malformed testplan.json" do
-      aggregate_failures do
-        expect(described_class.test_ids).to eq(nil)
-        expect(described_class.test_names).to eq(nil)
-      end
+      expect(described_class.test_ids).to eq(nil)
+      expect(described_class.test_names).to eq(nil)
     end
   end
 
   context "handles" do
-    let(:path) { nil }
+    let(:path) { "#{Dir.pwd}/spec/fixtures/test_plan" }
 
-    it "missing testplan.json" do
-      aggregate_failures do
-        expect(described_class.test_ids).to eq(nil)
-        expect(described_class.test_names).to eq(nil)
-      end
+    it "incorrect test plan directory" do
+      expect(described_class.test_ids).to eq(nil)
+      expect(described_class.test_names).to eq(nil)
+    end
+  end
+
+  context "handles" do
+    let(:path) { "#{Dir.pwd}/spec/fixtures/test_plan/correct" }
+
+    it "testplan.json directory" do
+      expect(described_class.test_ids).to eq(test_ids)
+      expect(described_class.test_names).to eq(test_names)
     end
   end
 end
